b2070510a03c4917b440197b14ff21634f8ac76e
Aryan Chopra <chopraryan@gmail.com>
2021-07-05 19:38:20 +0530

    test: favourite blog

---
+++ Part 4/Blog list/tests/dummy.test.js
@@ -0,0 +1,62 @@
+const totallikes = require("../utils/testHelper").totallikes;
+const favblog = require("../utils/testHelper").favblog;
+// describe("totallikes", () => {
+//   test("empty list returns 0", () => {
+//     const blogs = [];
+//     console.log(typeof totallikes(blogs));
+//     expect(totallikes(blogs)).toBe(0);
+//   });
+//   test("fulllist", () => {
+//     const blogs = [
+//       {
+//         _id: "5a422a851b54a676234d17f7",
+//         title: "React patterns",
+//         author: "Michael Chan",
+//         url: "https://reactpatterns.com/",
+//         likes: 7,
+//         __v: 0,
+//       },
+//       {
+//         _id: "5a422aa71b54a676234d17f8",
+//         title: "Go To Statement Considered Harmful",
+//         author: "Edsger W. Dijkstra",
+//         url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+//         likes: 5,
+//         __v: 0,
+//       },
+//     ];
+
+//     expect(totallikes(blogs)).toBe(12);
+//   });
+// });
+
+describe("favourite blogs", () => {
+  test("multiple blogs", () => {
+    const blogs = [
+      {
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0,
+      },
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0,
+      },
+    ];
+    expect(favblog(blogs)).toEqual({
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0,
+    });
+  });
+});
--- Part 4/Blog list/utils/testHelper.js
+++ Part 4/Blog list/utils/testHelper.js
@@ -1,7 +1,22 @@
-const dummy = (blogs) => {
-  return 1;
+const totallikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0;
+  }
+  const likes = blogs.map((blog) => blog.likes);
+  const reducer = (likes, val) => likes + val;
+  return likes.reduce(reducer, 0);
 };
 
+const favblog = (blogs) => {
+  const reducer = (prev, curr) => {
+    console.log("acc: ", prev, " curr: ", curr);
+    return prev.likes > curr.likes ? prev : curr;
+  };
+  const blogfav = blogs.reduce(reducer, 0);
+
+  return blogfav;
+};
 module.exports = {
-  dummy,
+  totallikes,
+  favblog,
 };
